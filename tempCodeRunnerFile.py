7f4e609516d18ad3eafb911a7a702086d183dba3
PBB999 <pratikbulkunde4@gmail.com>
2025-08-06 11:23:14 +0530

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download("punkt")
+nltk.download("stopwords")
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+worf_quote = "Sir, I protest. I am not a merry man!"
+words_in_quote = word_tokenize(worf_quote)
+print(words_in_quote)
